d4fa7217726f9d9a6b10c11da9b5fb6ab22ee23c
Jacob's-ROG-Laptop <shockley_3@msn.com>
2018-07-18 16:48:37 -0600

    Movment Changes & Quality of Life -Modify Movement & add animations -Add Time Counter -Add Game Over Screen -Refactor Resources

---
+++ Assets/Scripts/Player/GameOverScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 30, 100, 20), "Game Over");
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 200, 20), "Try Again"))
+            SceneManager.LoadScene(0);
+
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,35 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     float speed = 1;
+    Animator animator;
     Rigidbody2D rigidbody;
     AudioSource audioSource;
     [SerializeField]
     int health = 10;
+    TimeSpan time;
 	// Use this for initialization
 	void Start () {
+        animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        time += TimeSpan.FromSeconds(Time.deltaTime);
+
+        Vector3 pos = transform.position;
+        if (pos.x < -10)
+            pos.x = -10;
+        else if (pos.x > 10)
+            pos.x = 10;
+        transform.position = pos;
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
         Vector3 rot = transform.eulerAngles;
-        rot.z = h * -80;
+
+        rot.z = h * -30;
         transform.eulerAngles = rot;
+        animator.SetFloat("Rotation", h);
         Vector3 velocity = Vector3.up;
         velocity += transform.up;
         if(v < 0)
-            rigidbody.velocity = velocity * speed*0.5f;
+            rigidbody.velocity = velocity.normalized * speed*0.5f;
         else
-            rigidbody.velocity = velocity * speed;
+            rigidbody.velocity = velocity.normalized * speed;
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -37,7 +52,8 @@ public class PlayerController : MonoBehaviour {
         Debug.Log("Crashed");
         if (!collision.gameObject.CompareTag("PowerUp"))
         {
-            TakeDamage(collision.otherRigidbody.mass);
+            Debug.Log(collision.rigidbody.gameObject.name);
+            TakeDamage(collision.rigidbody.mass);
         }
     }
     void TakeDamage(float damage)
@@ -51,7 +67,11 @@ public class PlayerController : MonoBehaviour {
     }
     void GameOver()
     {
-        
-        SceneManager.LoadScene(0);
+        this.enabled = false;
+        GetComponent<GameOverScript>().enabled = true;
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 20), string.Format("{0}:{1}:{2}", time.Minutes, time.Seconds, time.Milliseconds));
     }
 }
