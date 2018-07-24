5c2038cb8c7184a24ced0fce54e7cadf31bd52fe
DoktorBeta <betababy@igame3d.com>
2018-07-24 12:20:29 -0600

    James' Scripts, DoktorBeta Sprites
    This update is now compatible to last week's meeting. Animation, Ship Sprites. etc. The ship collision mesh is not the same as James's. Need to update later.

--- Assets/Scripts/Player/GameOverScript.cs
+++ Assets/Scripts/Player/GameOverScript.cs
@@ -4,12 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
+	Scene scene;
+	PlayerController playercontroller;
+	GameObject ship;
 
-    // Use this for initialization
-    void Start()
+	// Use this for initialization
+	void Start()
     {
-
-    }
+		scene = SceneManager.GetActiveScene();
+		ship = GameObject.Find("Ship");
+	}
 
     // Update is called once per frame
     void Update()
@@ -18,10 +22,12 @@ public class GameOverScript : MonoBehaviour
     }
     public void OnGUI()
     {
-        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 30, 100, 20), "Game Over");
-        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 200, 20), "Try Again"))
-            SceneManager.LoadScene(0);
-
+		if (ship.GetComponent<PlayerController>().health <= 0)
+		{
+			GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
+			GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 30, 100, 20), "Game Over");
+			if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 10, 200, 20), "Try Again"))
+				SceneManager.LoadScene(scene.name);
+		}
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,62 +2,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     float speed = 1;
+    int hConstraint = 2;
+    float invulTime = 1;       //invulnerability - measured in seconds
+    float invulTimer = 0;
+    bool hit = false;
+    bool active = true;
+
     Animator animator;
     Rigidbody2D rigidbody;
     AudioSource audioSource;
-    [SerializeField]
-    int health = 10;
+    SpriteRenderer sprite;
+
+    [SerializeField] Collider2D noseCollider;
+
+    public int health = 10;
+
     TimeSpan time;
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         time += TimeSpan.FromSeconds(Time.deltaTime);
+        invulTimer -= Time.deltaTime;
 
         Vector3 pos = transform.position;
-        if (pos.x < -10)
-            pos.x = -10;
-        else if (pos.x > 10)
-            pos.x = 10;
-        transform.position = pos;
+        if (active)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) health = 0; //kill button
+            if (pos.x < -hConstraint)
+                pos.x = -hConstraint;
+            else if (pos.x > hConstraint)
+                pos.x = hConstraint;
+            transform.position = pos;
+            animator.SetInteger("AnimState", 0);
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxis("Vertical");
 
-        Vector3 rot = transform.eulerAngles;
+            //Debug.Log(h);
+            if (h < 0f)
+            {
+                animator.SetInteger("AnimState", 1);
+            }
+            else if (h > 0f)
+            {
+                animator.SetInteger("AnimState", 2);
+            }
 
-        rot.z = h * -30;
-        transform.eulerAngles = rot;
-        animator.SetFloat("Rotation", h);
-        Vector3 velocity = Vector3.up;
-        velocity += transform.up;
-        if(v < 0)
-            rigidbody.velocity = velocity.normalized * speed*0.5f;
+
+            Vector3 rot = transform.eulerAngles;
+            rot.z = 0;
+            transform.eulerAngles = rot;
+            //Debug.Log(h);
+
+            Vector3 velocity = Vector3.up;
+            velocity.x = h;
+            velocity += transform.up;
+            if (v < 0)
+                rigidbody.velocity = velocity.normalized * speed * 0.5f;
+            else
+                rigidbody.velocity = velocity.normalized * speed;
+            if (invulTimer <= 0) hit = false;
+
+        }
         else
-            rigidbody.velocity = velocity.normalized * speed;
+        {
+            sprite.enabled = false;
+        }
+
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Crashed");
-        if (!collision.gameObject.CompareTag("PowerUp"))
+        // Debug.Log("Crashed");
+        if (!collision.gameObject.CompareTag("PowerUp") && !hit)
         {
-            Debug.Log(collision.rigidbody.gameObject.name);
-            TakeDamage(collision.rigidbody.mass);
+            // Debug.Log(collision.rigidbody.gameObject.name);
+            if (rigidbody.transform.position.x > collision.rigidbody.transform.position.x)
+                collision.rigidbody.velocity += new Vector2(-2 / collision.rigidbody.mass, speed);
+            else collision.rigidbody.velocity += new Vector2(2 / collision.rigidbody.mass, speed);
+
+            // Check which hitbox is being hit then damage ship
+            float damageScale = collision.otherCollider == noseCollider ? 1f : 0.5f;
+            TakeDamage(collision.rigidbody.mass * damageScale);
         }
     }
     void TakeDamage(float damage)
     {
+        hit = true;
+        invulTimer = invulTime;
         health -= Mathf.CeilToInt(damage);
         audioSource.Play();
         if (health <= 0)
@@ -67,11 +112,12 @@ public class PlayerController : MonoBehaviour {
     }
     void GameOver()
     {
-        this.enabled = false;
         GetComponent<GameOverScript>().enabled = true;
+        active = false;
     }
     void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 200, 20), string.Format("{0}:{1}:{2}", time.Minutes, time.Seconds, time.Milliseconds));
+	Scene scene = SceneManager.GetActiveScene();
+        GUI.Label(new Rect(10, 10, 200, 20), string.Format("time: {0}:{1}:{2} // health: {3}", time.Minutes, time.Seconds, time.Milliseconds,health));	
     }
 }
