6e4f336051d8cc3ed52db074967b2657869959fb
DoktorBeta <betababy@igame3d.com>
2018-08-07 14:40:00 -0600

    Shop pause
    Added shop script
    Added shop UI
    Added shop music
    Fixed "enviorment" typo

---
+++ Assets/Scripts/Game/Shop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    [SerializeField] private GameObject shopPanel;
+    // Use this for initialization
+    void Start()
+    {
+        shopPanel.SetActive(false);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Shop"))
+        {
+            if (!shopPanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            else if (shopPanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+        else return;
+    }
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        shopPanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        shopPanel.SetActive(false);
+        //enable the scripts again
+    }
+}
